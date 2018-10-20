c2d6ce4ea4991ff087986bd3b937ec177dcc2bc8
Ulrike Liss <32239198+UlrikeLiss@users.noreply.github.com>
2018-10-20 20:26:19 +0200

    Update item.js

--- item.js
+++ item.js
@@ -1,1 +1,9 @@
-
+module.exports = class Item{
+    constructor(name, count) {
+        this.name = name
+        this.count = count
+    }
+    add(list) {
+        list.items.push(this)
+    }
+  }
