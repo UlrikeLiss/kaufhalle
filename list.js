36ed23673774fc75b6abbd95f0582b02c073db8f
Ulrike Liss <32239198+UlrikeLiss@users.noreply.github.com>
2018-10-20 20:26:46 +0200

    Update list.js

--- list.js
+++ list.js
@@ -1,1 +1,12 @@
-
+module.exports = class List {
+    constructor(name) {
+        this.name = name
+        this.items = []
+    }
+    add(mainList) {
+        mainList.lists.push(this)
+    }
+    report() {
+        console.log(this.name, this.items)
+    }
+  }
