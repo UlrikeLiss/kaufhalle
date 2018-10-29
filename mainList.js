1fedd447773340c0d4f959c065cfe050ccba68fb
Ulrike Liss <32239198+UlrikeLiss@users.noreply.github.com>
2018-10-29 20:01:55 +0100

    Update mainList.js

--- mainList.js
+++ mainList.js
@@ -1,3 +1,4 @@
+const List = require('./list')
 module.exports = class MainList {
     constructor(name) {
         this.name = name
@@ -9,5 +10,11 @@ module.exports = class MainList {
 
     report() {
         console.log(this.name, this.lists, this.lists.length)
+    }
+    static create({ name, lists, items }) {
+        const mainList = new MainList(name)
+        mainList.lists = lists.map(MainList.create)
+        list.items = items.map(List.create)
+        return mainList
     }
   }
