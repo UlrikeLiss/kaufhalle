eb4b8399460b0e68bbc046f85ed6303ad288139e
Ulrike Liss <32239198+UlrikeLiss@users.noreply.github.com>
2018-10-29 19:59:26 +0100

    Update database.js

--- database.js
+++ database.js
@@ -1,10 +1,26 @@
+
 const fs = require('fs')
+
+async function save(data) {
+    return new Promise((resolve, reject) => {
+        fs.writeFile('./data.json', JSON.stringify(data), (err, file) => {
+            if (err) return reject(err)
+            resolve()
+        })
+    })
+}
+    
+async function load(dbPath) {
+    return new Promise((resolve, reject) => {
+        fs.readFile(dbPath, 'utf8', (err, file) => {
+            if (err) return reject(err)
+            const data = JSON.parse(file)
+            resolve(data)
+       })
+    })
+}
+    
 module.exports = {
-        save:function(data) {
-            //console.log(data)
-            fs.writeFileSync('data.json', JSON.stringify(data))
-        },
-        load() {
-            return JSON.parse(fs.readFileSync('data.json'))
-        }
-    }
+    save,
+    load
+}
