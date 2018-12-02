4a8f5434456b399e88f3a978f35111789f11b180
Ulrike Liss <32239198+UlrikeLiss@users.noreply.github.com>
2018-12-02 16:57:37 +0100

    Add files via upload

---
+++ backendAndFrontend/Frontend/src/components/List.vue
@@ -0,0 +1,20 @@
+<script>
+export default {
+    name: 'list',
+    props: ['data']
+}
+</script>
+
+<template lang="pug">
+div
+    div {{data.name}}
+</template>
+
+<style>
+.wrapper {
+    background: red;
+    padding: 20px;
+    border: 1px solid;
+    border-radius: 20px;
+}
+</style>
