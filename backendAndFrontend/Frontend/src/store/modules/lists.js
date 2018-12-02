ad573164b242347ab6471c90994a600878b6e527
Ulrike Liss <32239198+UlrikeLiss@users.noreply.github.com>
2018-12-02 17:00:37 +0100

    Add files via upload

---
+++ backendAndFrontend/Frontend/src/store/modules/lists.js
@@ -0,0 +1,39 @@
+import axios from 'axios';
+
+const REQUEST_SUCCESS = 'REQUEST_SUCCESS';
+
+const state = {
+  data: [],
+  list: {}
+};
+
+const mutations = {
+  [REQUEST_SUCCESS](state, data) {
+    state.data = data;
+  },
+
+['REQUEST_LIST_SUCCESS'] (state, data){
+   state.list = data;
+}
+
+};
+
+const actions = {
+  async fetchLists({ commit }) {
+    const res = await axios.get('http://localhost:3000/list/all');
+    commit(REQUEST_SUCCESS, res.data);
+  
+  },
+
+  async fetchList({ commit }, name) {
+    const res = await axios.get('http://localhost:3000/list/${name}/json');
+    console.log(res)
+    commit('REQUEST_LIST_SUCCESS', res.data)
+  },
+};
+
+export default {
+  state,
+  mutations,
+  actions,
+};
